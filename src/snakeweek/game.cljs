125a63d7100b4857195c1d01970e36134479466a
freddy <freddy.teuma@smartodds.co.uk>
2017-10-24 16:29:04 +0100

    Remove useless file

--- src/snakeweek/game.cljs
+++
@@ -1,7 +0,0 @@
-(ns snakeweek.game
-  (:require [play-cljs.core :as p]
-            [snakeweek.assets :as assets]
-            [snakeweek.maps :as maps]
-            [snakeweek.drawing :as d]
-            [goog.events :as events]))
-
